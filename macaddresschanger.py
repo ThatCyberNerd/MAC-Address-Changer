bcffe4303365a99fd388a3c17f6ae2347e357181
Jayesh A <111209763+ThatCyberNerd@users.noreply.github.com>
2023-12-10 06:36:40 -0500

    Update macaddresschanger.py

--- macaddresschanger.py
+++ macaddresschanger.py
@@ -7,12 +7,12 @@ def get_arguments():
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
     (options, arguments) = parser.parse_args()
-    
     if not options.interface:
         parser.error("[.] Please specify an interface, check --help for more info")
     elif not options.new_mac:
         parser.error("[.] Please specify a MAC Address, check --help for more info")
     return options
+    
 def change_mac(interface, new_mac):
     print("[+] Changing the MAC Address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
